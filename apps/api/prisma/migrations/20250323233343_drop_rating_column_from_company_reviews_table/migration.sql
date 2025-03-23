9a8264ac202436d68a8031352e06af9b1630ecd0
rach-syahputra <rach.syahputra@gmail.com>
2025-03-24 07:34:36 +0800

    feat(api): drop rating column from company_reviews table

---
+++ apps/api/prisma/migrations/20250323233343_drop_rating_column_from_company_reviews_table/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `rating` on the `company_reviews` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "company_reviews" DROP COLUMN "rating";
