7aa33dd011d0fc4f5f90f95a36560ab1d0f1afe7
rach-syahputra <rach.syahputra@gmail.com>
2025-03-25 12:18:47 +0800

    feat(web): add disable prop to form input component

--- apps/web/src/components/ui/form-input.tsx
+++ apps/web/src/components/ui/form-input.tsx
@@ -7,6 +7,7 @@ interface FormInputProps {
   name: string;
   value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  disabled?: boolean;
   errorMessage?: string;
 }
 
@@ -16,12 +17,19 @@ const FormInput: React.FC<FormInputProps> = ({
   name,
   value,
   onChange,
+  disabled,
   errorMessage,
 }) => {
   return (
     <div className="flex flex-col gap-2">
       <Label htmlFor={name}>{label}</Label>
-      <Input type={type} name={name} onChange={onChange} value={value} />
+      <Input
+        type={type}
+        name={name}
+        onChange={onChange}
+        value={value}
+        disabled={disabled}
+      />
       {errorMessage && <p className="text-sm text-red-500">{errorMessage}</p>}
     </div>
   );
