d710b6c4aabdb5f642503a73135bd23c2e3d482d
rach-syahputra <rach.syahputra@gmail.com>
2025-03-26 08:07:09 +0800

    feat(api): update error response status code in user middleware

--- apps/api/src/interfaces/middleware.interface.ts
+++ apps/api/src/interfaces/middleware.interface.ts
@@ -3,6 +3,7 @@ import { Request } from 'express';
 export interface UserToken {
   id: string;
   email: string;
+  jobId: string;
 }
 
 export interface UserRequest extends Request {
--- apps/api/src/middlewares/user.middleware.ts
+++ apps/api/src/middlewares/user.middleware.ts
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 
 import { JWT_ACCESS_SECRET } from '@/config';
 import { ResponseError } from '@/helpers/error';
-import { UserRequest, UserToken } from '@/interfaces/users/user.interface';
+import { UserRequest, UserToken } from '@/interfaces/middleware.interface';
 
 export function verifyToken(
   req: UserRequest,
@@ -13,10 +13,10 @@ export function verifyToken(
   try {
     const { authorization } = req.headers;
     const token = String(authorization || '').split('Bearer ')[1];
-    if (!token) throw new ResponseError(401, 'Unauthorized.');
+    if (!token) throw new ResponseError(401, 'Unauthenticated.');
 
     const verifiedUser = jwt.verify(token, JWT_ACCESS_SECRET);
-    if (!verifiedUser) throw new ResponseError(401, 'Unauthorized.');
+    if (!verifiedUser) throw new ResponseError(403, 'Unauthorized.');
 
     req.user = verifiedUser as UserToken;
 
